cfd53f8d677740ad3958ecb440b4d34a85671ebe
wfee2000 <ilmingwinnie19@gmail.com>
2023-11-19 16:39:54 +0100

    chore: add test data for appointments

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -6,24 +6,20 @@
 -- alter sequence myentity_seq restart with 4;
 
 insert into Person (first_name, last_name, email)
-values ('Johann', 'Maier', 'johann.maier@yahoo.com');
-insert into Person (first_name, last_name, email)
-values ('Herbert', 'Huber', 'herbert.huber@gmx.com');
-insert into Person (first_name, last_name, email)
-values ('Julia', 'Schneider', 'julia.schneider@outlook.com');
-insert into Person (first_name, last_name, email)
-values ('Paul', 'Schulz', 'paul.schulz@aol.com');
-insert into Person (first_name, last_name, email)
-values ('Anna', 'Struff', 'anna.struff@juno.com');
+values ('Johann', 'Maier', 'johann.maier@yahoo.com'),
+       ('Herbert', 'Huber', 'herbert.huber@gmx.com'),
+       ('Julia', 'Schneider', 'julia.schneider@outlook.com'),
+       ('Paul', 'Schulz', 'paul.schulz@aol.com'),
+       ('Anna', 'Struff', 'anna.struff@juno.com');
 insert into Customer (id, date_of_birth)
-values (1, '2012-12-23');
-insert into Customer (id, date_of_birth)
-values (2, '1980-10-11');
-insert into Customer (id, date_of_birth)
-values (3, '2010-09-05');
-insert into Customer (id, date_of_birth)
-values (4, '1970-04-15');
-insert into Customer (id, date_of_birth)
-values (5, '1975-06-30');
+values (1, '2012-12-23'),
+       (2, '1980-10-11'),
+       (3, '2010-09-05'),
+       (4, '1970-04-15'),
+       (5, '1975-06-30');
 insert into Instructor (id, hiring_date)
 values (4, '2000-05-24');
+insert into Appointment (name, date, duration, adress)
+values ('Yoga', '2022-11-09', 1800000000000, 'Sophiengutstraße'),
+       ('Kinderschwimmen', '2023-04-26', 1800000000000, 'Taubenmarkt'),
+       ('WATSU', '2022-08-15', 1800000000000, 'Goethestraße');
