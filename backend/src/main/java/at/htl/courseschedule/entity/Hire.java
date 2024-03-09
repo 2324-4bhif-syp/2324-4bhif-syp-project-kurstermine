0bea736ac00dadc7f3b3fa3e2581ac49aab8535c
Bajtik0905 <bergbajtik@gmail.com>
2024-03-09 18:50:11 +0100

    fix: added missing annotations in Hire Class

--- backend/src/main/java/at/htl/courseschedule/entity/Hire.java
+++ backend/src/main/java/at/htl/courseschedule/entity/Hire.java
@@ -1,9 +1,9 @@
 package at.htl.courseschedule.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "hire")
 public class Hire {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
