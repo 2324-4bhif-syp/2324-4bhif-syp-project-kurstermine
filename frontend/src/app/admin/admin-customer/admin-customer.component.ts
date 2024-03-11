5480153b0bf0dff70d1ad18f245d27dc470316f5
Austromo <moritz.gruber.linz@gmail.com>
2024-03-11 18:16:34 +0100

    fix: outsource packet html-code #244bkt-95 To Verify

--- frontend/src/app/admin/admin-customer/admin-customer.component.ts
+++ frontend/src/app/admin/admin-customer/admin-customer.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { MatActionList, MatListModule } from '@angular/material/list';
+import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
 import { ActivatedRoute } from '@angular/router';
 import { CustomerService } from 'src/shared/services/customer.service';
 import { PacketService } from 'src/shared/services/packet.service';
@@ -7,16 +7,15 @@ import { PurchaseService } from 'src/shared/services/purchase.service';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { AppointmentService } from 'src/shared/services/appointment.service';
 import { OfferService } from 'src/shared/services/offer.service';
-import { ParticipationService } from 'src/shared/services/participation.service';
-import { Appointment } from 'src/shared/models/appointment';
 import { MatIconModule } from '@angular/material/icon';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatCardModule } from '@angular/material/card';
+import {AdminPacketComponent} from "../admin-packet/admin-packet.component";
 
 @Component({
   selector: 'app-admin-customer',
   standalone: true,
-  imports: [MatListModule, MatExpansionModule, MatIconModule, MatChipsModule, MatCardModule],
+    imports: [MatListModule, MatExpansionModule, MatIconModule, MatChipsModule, MatCardModule, AdminPacketComponent],
   templateUrl: './admin-customer.component.html',
   styleUrl: './admin-customer.component.css'
 })
@@ -27,7 +26,6 @@ export class AdminCustomerComponent {
     protected purchaseService: PurchaseService,
     protected appointmentService: AppointmentService,
     protected offerService: OfferService,
-    protected participationService: ParticipationService,
     private route: ActivatedRoute,
   ) {
 
@@ -58,15 +56,4 @@ export class AdminCustomerComponent {
   protected get offers() {
     return this.offerService.get();
   }
-
-  protected getAppointmentsForPacket(packetId: number) {
-    return this.offers.filter(o => o.packet.id === packetId).map(o => o.appointment);
-  }
-
-  protected isUserParticipatingInAppointment(appointment: Appointment) {
-    return this.participationService.get(p => p.customer.id === this.id
-      && p.appointment.id === appointment.id).length > 0;
-  }
-
-  public panelOpenState: boolean = false;
 }
--- frontend/src/app/admin/admin-packet/admin-packet.component.ts
+++ frontend/src/app/admin/admin-packet/admin-packet.component.ts
@@ -7,6 +7,11 @@ import {MatListModule} from "@angular/material/list";
 import {MatCardModule} from "@angular/material/card";
 import {OfferService} from "../../../shared/services/offer.service";
 import {Appointment} from "../../../shared/models/appointment";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatButtonModule} from "@angular/material/button";
+import {ParticipationService} from "../../../shared/services/participation.service";
+import {ActivatedRoute} from "@angular/router";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-admin-packet',
@@ -18,22 +23,38 @@ import {Appointment} from "../../../shared/models/appointment";
         FormsModule,
         MatExpansionModule,
         MatListModule,
-        MatCardModule
+        MatCardModule,
+        MatChipsModule,
+        MatButtonModule,
+        MatIconModule
     ],
   templateUrl: './admin-packet.component.html',
   styleUrl: './admin-packet.component.css'
 })
 export class AdminPacketComponent {
-    constructor(protected offerService: OfferService) {
+    constructor(protected offerService: OfferService,
+                protected participationService: ParticipationService,
+                private route: ActivatedRoute) {
     }
 
     @Input({required: true})
     public packet!: Packet;
-    public panelOpenState: boolean = false;
 
-    getAllAppointmentsFromPacket(): Appointment[] {
+    @Input({required: true})
+    public displaySignedInBtn!: boolean;
+
+    protected panelOpenState: boolean = false;
+
+    protected id = this.route.snapshot.params['id'];
+
+    protected getAllAppointmentsFromPacket(): Appointment[] {
         return this.offerService
             .get(o => o.packet.id === this.packet.id)
             .map(o => o.appointment);
+    }
+
+    protected isUserParticipatingInAppointment(appointment: Appointment): boolean {
+        return this.participationService
+            .get(p => p.customer.id === this.id && p.appointment.id === appointment.id).length > 0;
     }
 }
