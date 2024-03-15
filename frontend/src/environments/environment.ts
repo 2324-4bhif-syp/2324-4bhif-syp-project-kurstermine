9381619794af91b0952b6ab5e0611820cf42993f
wfee2000 <ilmingwinnie19@gmail.com>
2024-03-15 19:52:01 +0100

    ci: automatically configure for deployment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,7 +2,4 @@ export const environment = {
     angularUrl: 'http://localhost',
     apiUrl: 'http://localhost:8080',
     keycloakUrl: 'http://localhost:8180',
-    //angularUrl: 'https://babyapp.ddns.net',
-    //apiUrl: 'https://babyapp.ddns.net/api',
-    //keycloakUrl: 'https://babyapp.ddns.net/keycloak',
 };
