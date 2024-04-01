339d1b865c792853fd0a6e3d219deeb1dd3f8664
veqox <sonnleitner.luki@gmail.com>
2024-04-01 16:16:28 +0200

    feat: add a database to keycloak #244bkt-99 To Verify feat: optimize keycloak build

--- keycloak/keycloak-test.sh
+++
@@ -1,6 +0,0 @@
-docker run -p 8180:8080 \
-        --name keycloak-test \
-        -e KEYCLOAK_ADMIN=admin \
-        -e KEYCLOAK_ADMIN_PASSWORD=admin \
-        -v .:/opt/keycloak/data/import \
-        quay.io/keycloak/keycloak:23.0.0 start-dev --import-realm --spi-theme-static-max-age=-1 --spi-theme-cache-themes=false --spi-theme-cache-templates=false
