8e922315d7db0b770bbe64c085f12b9da0165884
wfee2000 <ilmingwinnie19@gmail.com>
2024-04-01 17:20:39 +0200

    refactor: remove unused properties of User

--- backend/src/main/java/at/htl/courseschedule/entity/User.java
+++ backend/src/main/java/at/htl/courseschedule/entity/User.java
@@ -5,55 +5,35 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.UUID;
+
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String username;
-    private String password;
-    private String role;
+    private UUID uuid;
 
     public User() {
     }
 
-    public User(String username, String password, String role) {
-        this.username = username;
-        this.password = password;
-        this.role = role;
+    public User(UUID uuid) {
+        this.uuid = uuid;
     }
 
-    public String getUsername() {
-        return this.username;
+    public UUID getUuid() {
+        return this.uuid;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return this.role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
     }
 
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", role='" + role + '\'' +
+                ", uuid=" + uuid +
                 '}';
     }
 
