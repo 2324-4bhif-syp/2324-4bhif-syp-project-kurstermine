6ea18340ff87d17f0582fe8cd4b5be1f6acf4c2c
Austromo <moritz.gruber.linz@gmail.com>
2024-04-02 11:41:52 +0200

    feat: add email html template

--- backend/src/main/java/at/htl/courseschedule/controller/MailService.java
+++ backend/src/main/java/at/htl/courseschedule/controller/MailService.java
@@ -2,24 +2,27 @@ package at.htl.courseschedule.controller;
 
 import io.quarkus.mailer.Mail;
 import io.quarkus.mailer.reactive.ReactiveMailer;
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class MailService {
+    @CheckedTemplate(requireTypeSafeExpressions = false)
+    static class ConfirmationTemplate {
+        static native TemplateInstance confirm(String packetName, String receiver);
+    }
+
     @Inject
     ReactiveMailer reactiveMailer;
 
     public Uni<Void> sendConfirmationMail(String email, String packetName, String receiver) {
-        return sendMail(Mail.withText(
+        return sendMail(Mail.withHtml(
                 email,
-                String.format("Confirmation for %s", packetName),
-                String.format(
-                        "Dear %s! \n" +
-                                "Your purchase of the package %s was successful!",
-                        receiver, packetName
-                )
+                "Confirmation for Purchase",
+                ConfirmationTemplate.confirm(packetName, receiver).render()
         ));
     }
 
