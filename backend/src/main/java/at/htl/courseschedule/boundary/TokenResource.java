70e035b86aa06b3d72fc71db99886ece383a03c8
Bajtik0905 <bergbajtik@gmail.com>
2024-10-23 20:35:31 +0200

    feat: added Token endpoints

--- backend/src/main/java/at/htl/courseschedule/boundary/TokenResource.java
+++ backend/src/main/java/at/htl/courseschedule/boundary/TokenResource.java
@@ -1,10 +1,83 @@
 package at.htl.courseschedule.boundary;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
+import at.htl.courseschedule.controller.TokenRepository;
+import at.htl.courseschedule.entity.Token;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
 
-@Path("Tokens")
+import java.util.UUID;
+
+@Path("tokens")
 public class TokenResource {
+    @Inject
+    TokenRepository tokenRepository;
+
     @GET
-    public void get() {}
+    @Path("{token-id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed({Role.Admin, Role.Instructor, Role.Customer, Role.Organisator})
+    public Response getById(@PathParam("token-id") UUID tokenId) {
+        return Response.ok(tokenRepository.findById(tokenId)).build();
+    }
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed({Role.Admin, Role.Instructor, Role.Customer, Role.Organisator})
+    public Response createToken(Token token, @Context UriInfo uriInfo) {
+        if(token == null) {
+            return Response.status(400).build();
+        }
+
+        tokenRepository.persist(token);
+
+        UriBuilder uriBuilder = uriInfo
+                .getAbsolutePathBuilder()
+                .path(token.getId().toString());
+
+        return Response.created(uriBuilder.build()).entity(token).build();
+    }
+
+    @DELETE
+    @Path("{token-id}")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed({Role.Admin, Role.Organisator})
+    public Response deleteToken(@PathParam("token-id") UUID tokenId) {
+        Token token = tokenRepository.findById(tokenId);
+
+        if(token == null) {
+            return Response.status(404).build();
+        }
+
+        tokenRepository.delete(token);
+
+        return Response.ok(token).build();
+    }
+
+    @PUT
+    @Path("{token-id}")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed({Role.Admin, Role.Organisator})
+    public Response updateToken(@PathParam("token-id") UUID tokenId, Token token) {
+        Token oldToken = tokenRepository.findById(tokenId);
+
+        if(oldToken == null) {
+            return Response.status(404).build();
+        }
+
+        oldToken.setCategory(token.getCategory());
+        oldToken.setAppointment(token.getAppointment());
+
+        tokenRepository.persist(oldToken);
+
+        return Response.ok(oldToken).build();
+    }
 }
