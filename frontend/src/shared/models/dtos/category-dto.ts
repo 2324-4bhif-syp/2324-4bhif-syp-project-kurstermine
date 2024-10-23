1062d8194a4b03ed32e4959e06cc4de2545fdbf7
Austromo <moritz.gruber.linz@gmail.com>
2024-10-23 23:18:22 +0200

    feat: add dtos for new interfaces #244bkt-137

---
+++ frontend/src/shared/models/dtos/category-dto.ts
@@ -0,0 +1,19 @@
+import { Category } from "@models";
+
+export interface CategoryDto {
+    id: number;
+    name: string;
+    organisation: {
+        id: number;
+    };
+}
+
+export const fromCategory = (category: Category): CategoryDto => {
+    return {
+        id: category.id,
+        name: category.name,
+        organisation: {
+            id: category.organisationId
+        }
+    };
+};
---
+++ frontend/src/shared/models/dtos/token-dto.ts
@@ -0,0 +1,33 @@
+import { Token } from "@models";
+
+export interface TokenDto {
+    id: string; // UUID
+    appointment: {
+        id?: number;
+    };
+    category: {
+        id: number;
+    };
+    user: {
+        id: string; // UUID
+    };
+    purchasedAt: Date;
+    redeemedAt: Date;
+}
+
+export const fromToken = (token: Token): TokenDto => {
+    return {
+        id: token.id,
+        appointment: {
+            id: token.appointmentId
+        },
+        category: {
+            id: token.categoryId
+        },
+        user: {
+            id: token.userId
+        },
+        purchasedAt: token.purchasedAt,
+        redeemedAt: token.redeemedAt
+    };
+};
