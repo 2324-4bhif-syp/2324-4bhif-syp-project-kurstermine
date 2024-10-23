80d9a884210a4f7fd37af08e154bfcb600122759
veqox <veqoxao@gmail.com>
2024-10-23 19:38:51 +0200

    feat(scripts): create log files for each process

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -3,34 +3,50 @@ BACKEND_HOME=backend
 FRONTEND_HOME=frontend
 POSTGRES_HOME=backend
 
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+mkdir -p logs
+
 pushd $KEYCLOAK_HOME
-docker compose up -d
+docker compose up -d &> ../logs/keycloak.log
+docker compose ls -q
 popd
 
 pushd $POSTGRES_HOME
-./postgres-start.sh
+./postgres-start.sh &> ../logs/postgres.log
+pushd db-postgres
+docker compose ls -q
+popd
 popd
 
 pushd $BACKEND_HOME
-quarkus dev --clean &
+quarkus dev --clean &> ../logs/backend.log &
 BACKEND_PID=$!
+echo "Backend: $BACKEND_PID"
 popd
 
 pushd $FRONTEND_HOME
-npm i
-npm start &
+npm start &> ../logs/frontend.log &
 FRONTEND_PID=$!
+echo "Frontend: $FRONTEND_PID"
 popd
 
 echo "Press any key to stop"
 read -n 1
+echo "Stopping..."
 
 pushd $POSTGRES_HOME
-./postgres-stop.sh
+./postgres-stop.sh &>> ../logs/postgres.log
 popd
 
 pushd $KEYCLOAK_HOME
-docker compose down
+docker compose down &>> ../logs/keycloak.log
 popd
 
 kill $BACKEND_PID
