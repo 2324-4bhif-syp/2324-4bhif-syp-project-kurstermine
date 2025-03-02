027f58213623d6185cc2eac9d6893864bfeb7478
Bajtik0905 <bergbajtik@gmail.com>
2025-03-02 22:22:29 +0100

    fix: added organisation field to AdminUser interface #244bkt-159

--- frontend/src/shared/models/admin-user.ts
+++ frontend/src/shared/models/admin-user.ts
@@ -1,7 +1,10 @@
+import {Organisation} from "@models/organisation";
+
 export interface AdminUser {
   id?: string;
   firstName: string;
   lastName: string;
   email: string;
+  organisation: Organisation | null;
   roles: string[];
 }
